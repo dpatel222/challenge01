928b2c17a5b444e51157af2a4221379fa0f22be9
dpatel222 <deeppatel2@rrc.ca>
2024-05-15 11:18:06 -0500

    no space btween the period and word

--- carl_sagan.rb
+++ carl_sagan.rb
@@ -23,5 +23,4 @@ sagan = [
 #puts "#{time[:that]} #{example[2]} #{example[1]} #{example[0]}#{time[:period]}"
 
 
-puts "#{carl[:words][2]} #{sagan[0][:are]} #{sagan[0]["A"]} #{sagan[1]["waaaaaay"]} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]} #{carl[:punctuation][1]}".capitalize
-
+puts "#{carl[:words][2]} #{sagan[0][:are]} #{sagan[0]["A"]} #{sagan[1]["waaaaaay"]} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}#{carl[:punctuation][1]}".capitalize 
